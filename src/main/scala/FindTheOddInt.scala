6b7b5226e070b47e3969869a2d0885e73336cb0e
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-03-29 19:04:19 +0100

    Refactor FindTheOddInt

--- src/main/scala/FindTheOddInt.scala
+++ src/main/scala/FindTheOddInt.scala
@@ -1,5 +1,5 @@
 object FindTheOddInt {
   def findOdd(xs: Seq[Int]): Int = {
-    xs.find(int => (xs.count(_ == int) % 2) != 0).get
+    xs.find(int => (xs.count(_ == int) % 2) != 0).getOrElse(0)
   }
 }
