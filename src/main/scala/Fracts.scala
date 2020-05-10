bad5bd330bab9704dbb1324bf90d10b63b507863
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-05-10 12:25:53 +0100

    Still bad, but made variable names meaningful

--- src/main/scala/Fracts.scala
+++ src/main/scala/Fracts.scala
@@ -1,34 +1,34 @@
-  object Fracts {
+object Fracts {
 
-    def convertFrac(lst: Array[(Long, Long)]): String = {
-      val simplifiedFraction = lst.toList.map(simplify(2,_))
-      val denominatorList = simplifiedFraction.map(_._2)
-      val leastDenominator = lcd(denominatorList.min, denominatorList)
-      simplifiedFraction.map {
-        fraction =>
-          val multiply = leastDenominator / fraction._2
-          (fraction._1 * multiply, fraction._2 * multiply)
+  def convertFrac(lst: Array[(Long, Long)]): String = {
+    val reducedFraction = lst.toList.map(fraction => reduceFraction(2, fraction._1, fraction._2))
+    val denominatorList = reducedFraction.map(_._2)
+    val leastDenominator = lcd(denominatorList.min, denominatorList)
+    reducedFraction.map {
+      fraction =>
+        val multiplyBy = leastDenominator / fraction._2
+        (fraction._1 * multiplyBy, fraction._2 * multiplyBy)
+    }.mkString
+  }
 
-      }.mkString
-    }
-
-    @scala.annotation.tailrec
-    def lcd(seedValue: Long, denominatorList: List[Long]): Long = {
-      denominatorList.find(seedValue % _ != 0) match {
-        case None => seedValue
-        case _ => lcd(seedValue + denominatorList.min, denominatorList)
-      }
-    }
-
-    @scala.annotation.tailrec
-    def simplify(seedValue: Int, fraction: (Long, Long)): (Long, Long) = {
-       if (seedValue > fraction._1 ) {
-         fraction
-       } else if ((fraction._1 % seedValue == 0) && (fraction._2 % seedValue == 0)) {
-         simplify(seedValue, (fraction._1 / seedValue, fraction._2 / seedValue))
-       }
-       else {
-         simplify(seedValue + 1, fraction)
-      }
+  @scala.annotation.tailrec
+  def lcd(leastDenominator: Long, denominatorList: List[Long]): Long = {
+    denominatorList.find(leastDenominator % _ != 0) match {
+      case None => leastDenominator
+      case _ => lcd(leastDenominator + denominatorList.min, denominatorList)
     }
   }
+
+  @scala.annotation.tailrec
+  def reduceFraction(divideByNumber: Int, numerator: Long, denominator: Long): (Long, Long) = {
+
+    if (divideByNumber > numerator) {
+      (numerator, denominator)
+    } else if ((numerator % divideByNumber == 0) && (denominator % divideByNumber == 0)) {
+      reduceFraction(divideByNumber, numerator / divideByNumber, denominator / divideByNumber)
+    }
+    else {
+      reduceFraction(divideByNumber + 1, numerator, denominator)
+    }
+  }
+}
