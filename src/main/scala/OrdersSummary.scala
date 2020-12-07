9cd94a603b2b16726792c2c37c5fe58c4523eb7d
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-12-07 21:14:08 +0000

    OrdersSummary - Refactored

--- src/main/scala/OrdersSummary.scala
+++ src/main/scala/OrdersSummary.scala
@@ -1,12 +1,17 @@
 import scala.util.matching.Regex
 
-//TODO: Refactor
 object OrdersSummary {
-  val orderPattern: Regex = "(^\\w+\\.?\\w+)\\s([0-9]+)\\s([0-9]+\\.[0-9]+)\\s([B|S])".r
+  val stockPattern = "(^\\w+\\.?\\w+)"
+  val quantityPattern = "([0-9]+)"
+  val pricePattern = "([0-9]+\\.[0-9]+)"
+  val orderType = "([B|S])"
+  val orderPattern: Regex = s"$stockPattern\\s$quantityPattern\\s$pricePattern\\s$orderType".r
+  val buy: String = "B"
+  val sell: String = "S"
 
-  def buySellFunc: (String, String) => Double = { (order, orderType) =>
+  def findTransactions: (String, String) => Double = { (order, orderType) =>
     order.trim match {
-      case orderPattern(stock, quantity, price, status) if status == orderType =>
+      case orderPattern(_, quantity, price, status) if status == orderType =>
         quantity.toInt * price.toDouble
       case _ => 0
     }
@@ -14,8 +19,8 @@ object OrdersSummary {
 
   def balanceStatements(lst: String): String = {
     val orders: List[String] = lst.split(',').toList
-    val bought = orders.map(buySellFunc(_, "B")).sum.round
-    val sold = orders.map(buySellFunc(_, "S")).sum.round
+    val bought = orders.map(findTransactions(_, buy)).sum.round
+    val sold = orders.map(findTransactions(_, sell)).sum.round
     val badOrders = orders.filterNot(_.trim.matches(orderPattern.regex)).map(_.trim)
     if (badOrders.isEmpty || badOrders.flatten.isEmpty)
       s"Buy: $bought Sell: $sold"
