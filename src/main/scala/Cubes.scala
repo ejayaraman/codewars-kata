466f2c5a0ed5d3abd701afa3c987dd304c3b504b
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-12-15 19:15:45 +0000

    CubesTest - Refactored

--- src/main/scala/Cubes.scala
+++ src/main/scala/Cubes.scala
@@ -4,13 +4,18 @@ object Cubes {
   def isSumOfCubes(s: String): String = {
     val pattern: Regex = "(\\d{1,3})".r
 
-    def isCubic: String => Boolean =  {
-      num:String => num.toCharArray.map(x => x.asDigit).map(x=>x*x*x).sum == num.toInt
+    def isCubic: String => Boolean = { num: String =>
+      num.toCharArray.map(x => x.asDigit).map(Math.pow(_,3)).sum == num.toInt
     }
 
-    pattern.findAllMatchIn(s).mkString(" ").split(' ').filter(isCubic) match {
-      case Array() => "Unlucky"
-      case cubicList => s"${cubicList.map(_.toInt).mkString(" ")} ${cubicList.map(_.toInt).sum} Lucky"
+    pattern
+      .findAllMatchIn(s)
+      .map(_.toString())
+      .filter(isCubic)
+      .map(_.toInt)
+      .toList match {
+      case List()    => "Unlucky"
+      case cubicList => s"${cubicList.mkString(" ")} ${cubicList.sum} Lucky"
     }
   }
 }
